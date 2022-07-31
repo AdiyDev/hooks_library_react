9e59f8a3a3cba177b815c49af634adfd5c3688db
Arkadiy B <adiy.work.mail@ya.ru>
2022-08-01 01:04:17 +0300

    feat: clicn feat: confirm feat: fade in feat: use network feat: notifications

---
+++ useClick/useClick.js
@@ -0,0 +1,23 @@
+import { useEffect } from 'react'
+
+export const useClick = onClick => {
+  if (typeof onClick !== 'function') {
+    return
+  }
+
+  const element = useRef()
+
+  useEffect(() => {
+    if (element.current) {
+      element.current.addEventListener('click', onClick)
+    }
+
+    return () => {
+      if (element.current) {
+        element.current.removeEventListener('click', onClick)
+      }
+    }
+  }, [])
+
+  return element
+}
