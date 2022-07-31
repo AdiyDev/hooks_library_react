dc1ab61568f5b1340d3d9ff7897b11f7ced5cc82
Arkadiy B <adiy.work.mail@ya.ru>
2022-08-01 00:51:52 +0300

    feat: add hook before leave page

---
+++ useBeforeLeave/useBeforeLeave.js
@@ -0,0 +1,17 @@
+export const useBeforeLeave = onBefore => {
+  if (typeof onBefore !== 'function') {
+    return
+  }
+
+  const handler = event => {
+    const { clientY } = event
+    if (clientY <= 0) {
+      onBefore()
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener('mouseleave', handler)
+    return () => document.removeEventListener('mouseleave', handler)
+  }, [])
+}
