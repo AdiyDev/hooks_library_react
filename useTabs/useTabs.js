2eaeada4c5ccc30d3f713f7bbaa06f0df50530c9
Arkadiy B <adiy.work.mail@ya.ru>
2022-08-01 00:32:59 +0300

    feat: add useTabs

---
+++ useTabs/useTabs.js
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+export const useTabs = (initialTab, allTabs) => {
+  if (!allTabs || !Array.isArray(allTabs)) {
+    return
+  }
+
+  const [currentIndex, setCurrentIndex] = useState(initialTab)
+
+  return {
+    currentItem: allTabs[currentIndex],
+    changeItem: setCurrentIndex
+  }
+}
